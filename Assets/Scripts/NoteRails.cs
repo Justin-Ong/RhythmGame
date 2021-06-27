64b7d199501a74748d2cd4ad3a3939901e439708
Justin-Ong <justinong32@gmail.com>
2021-06-27 20:47:52 +0800

    Add rail for notes to follow

---
+++ Assets/Scripts/NoteRails.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteRails : MonoBehaviour
+{
+    public Vector3 spawnPoint;
+    public GameObject NotePrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
